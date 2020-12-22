9846e6190550c7c35f03f7827a1608b1b403cdc4
9qq-qq-qq9 <46979929+9qq-qq-qq9@users.noreply.github.com>
2020-12-22 16:44:26 -0700

    testing mongodb stuff

--- app/src/main/java/me/greenpilot/vc/Listener.java
+++ app/src/main/java/me/greenpilot/vc/Listener.java
@@ -1,15 +1,19 @@
 package me.greenpilot.vc;
 
+import com.mongodb.*;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.managers.AudioManager;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import javax.annotation.Nonnull;
 import java.util.Objects;
 
@@ -24,15 +28,12 @@ public class Listener extends ListenerAdapter {
 
     @Override
     public void onGuildVoiceJoin(@Nonnull GuildVoiceJoinEvent event) {
-        if(event.getMember().getUser().isBot()) {
+        if(event.getMember().getUser().isBot())
             return;
-        }
 
-        if (!config.Get("TARGET").isEmpty()) {
-            if(!event.getMember().getUser().getId().equals(config.Get("TARGET"))) {
+        if (!config.Get("TARGET").isEmpty())
+            if(!event.getMember().getUser().getId().equals(config.Get("TARGET")))
                 return;
-            }
-        }
 
         VoiceChannel connectedChannel = Objects.requireNonNull(event.getMember().getVoiceState()).getChannel();
         AudioManager audioManager = event.getGuild().getAudioManager();
@@ -44,15 +45,12 @@ public class Listener extends ListenerAdapter {
 
     @Override
     public void onGuildVoiceMove(@Nonnull GuildVoiceMoveEvent event) {
-        if(event.getMember().getUser().isBot()) {
+        if(event.getMember().getUser().isBot())
             return;
-        }
 
-        if (!config.Get("TARGET").isEmpty()) {
-            if(!event.getMember().getUser().getId().equals(config.Get("TARGET"))) {
+        if (!config.Get("TARGET").isEmpty())
+            if(!event.getMember().getUser().getId().equals(config.Get("TARGET")))
                 return;
-            }
-        }
 
         VoiceChannel connectedChannel = Objects.requireNonNull(event.getMember().getVoiceState()).getChannel();
         AudioManager audioManager = event.getGuild().getAudioManager();
@@ -64,12 +62,36 @@ public class Listener extends ListenerAdapter {
 
     @Override
     public void onGuildVoiceLeave(@Nonnull GuildVoiceLeaveEvent event) {
-        if(event.getMember().getUser().isBot()) {
+        if(event.getMember().getUser().isBot())
             return;
-        }
 
         AudioManager audioManager = event.getGuild().getAudioManager();
 
         audioManager.closeAudioConnection();
+    }
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        MongoClient mongoClient = new MongoClient(new MongoClientURI(config.Get("MONGODB")));
+        //noinspection deprecation
+        DB database = mongoClient.getDB("antizook");
+        DBCollection collection = database.getCollection("695346842900234332");
+
+        String msg = event.getMessage().getContentRaw();
+
+        if (!msg.startsWith(".set target "))
+            return;
+
+        DBCursor cursor = collection.find();
+        while (cursor.hasNext()) {
+            DBObject obj = cursor.next();
+            String authorid = event.getAuthor().getId();
+            String admins = obj.get("ADMINS").toString();
+            if (!admins.contains(authorid)) {
+                LOGGER.info("User {} attempted to run a command but was blocked", event.getAuthor().getAsTag());
+            }
+        }
+
+        MessageChannel channel = event.getChannel();
     }
 }
